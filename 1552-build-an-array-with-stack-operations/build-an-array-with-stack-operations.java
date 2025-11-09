15b47878f4189ae59695f4d20cc9270718a9071d
nithees-sj <nitheessj@gmail.com>
2025-11-09 18:27:39 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (6.02%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        int stream = 1;
+        List<String> result = new ArrayList<>();
+        
+        int i = 0;
+        while (i < target.length && stream <= n) {
+            result.add("Push");
+            
+            if (stream == target[i]) {
+                i++;
+            } else {
+                result.add("Pop");
+            }
+            stream++;
+        }
+        
+        return result;
+    }
+}
